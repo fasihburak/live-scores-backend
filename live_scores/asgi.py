0691d661bf18504aa7895090da4a630b5e8238b6
Fasih Burak <fasihburak@gmail.com>
2025-06-23 11:01:07 +0300

    Limit the origins from which a websocket connection can be opened

--- live_scores/asgi.py
+++ live_scores/asgi.py
@@ -2,7 +2,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
@@ -15,8 +15,12 @@ from chat.routing import websocket_urlpatterns
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        "websocket": OriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+            [
+                "http://localhost:3000", # For react
+                "http://localhost:8000", # For django
+                ],
         ),
     }
 )
