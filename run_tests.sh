ec9867f651386ca1a00b0394ca90c6872e86bc98
Fasih Burak <fasihburak@gmail.com>
2025-08-06 12:07:52 +0300

    Update run.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 # Remove postgres and redis containers together with any persistent volumes defined for them.
 docker compose down -v
-docker compose run --rm django sh -c "/wait-for-it.sh postgres-db:5432 -- 
-python manage.py migrate && pytest"
+docker compose run --rm django sh -c \
+"/wait-for-it.sh postgres-db:5432 -- python manage.py migrate && pytest"
